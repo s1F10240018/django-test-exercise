559c6a35bc7ee6875cdf234e96cae48d18e1e9e3
1F10240018 <s1F102400189@iniad.org>
2025-07-03 11:36:27 +0900

    flake8修正

--- todo/tests.py
+++ todo/tests.py
@@ -9,6 +9,7 @@ class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -31,7 +32,7 @@ class TaskModelTestCase(TestCase):
 
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        current = timezone.make_aware(datetime(2024, 7,10, 0, 0, 0))
+        current = timezone.make_aware(datetime(2024, 7, 10, 0, 0, 0))
         task = Task(title='task1', due_at=due)
         task.save()
 
@@ -63,7 +64,7 @@ class TaskModelTestCase(TestCase):
 
         def test_index_post(self):
             client = Client()
-            data = {'title':'Test Task', 'due_at': '202-06-30 23:59:59'}
+            data = {'title': 'Test Task', 'due_at': '202-06-30 23:59:59'}
             response = client.post('/', data)
 
             self.assertEqual(response.status_code, 200)
@@ -95,4 +96,3 @@ class TaskModelTestCase(TestCase):
             self.assertEqual(response.templates[0].name, 'todo/index.html')
             self.assertEqual(response.context['tasks'][0], task2)
             self.assertEqual(response.context['tasks'][1], task1)
-    
